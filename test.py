22df5b229dc53fdac1512054cddab8619309ddb0
Tanuj Garg <tanujgarg@pop-os.localdomain>
2019-05-01 16:43:13 +0530

    l

--- test.py
+++ test.py
@@ -26,42 +26,42 @@ def crop_image(im, w, h):
     val3 = rowsum[idx3]
     val4 = rowsum[idx4]
     if val1 + val2 + val3 + val4 < 100000:
-    	return im
+        return im
     else:
-    	return im[idx1:idx2-idx1+w_mid, idx3:idx4-idx3+h_mid]
+        return im[idx1:idx2-idx1+w_mid, idx3:idx4-idx3+h_mid]
 def testing():
-	slides = []
-	ppt = []
-	ans_range = []
-	index = 0
-	for folder in os.listdir('Dataset'):
-		index_start = index
-		for filename in os.listdir('Dataset/'+folder):
-			img = cv.imread(os.path.join("Dataset",folder,filename))
-			# print(filename, folder)
-			imgray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-			print(img.shape, folder, filename)	
-			w,h,_=img.shape
-			edgy = edge_detection(imgray)
-			img = crop_image(edgy,w,h)
-			if filename == "ppt.jpg":
-				ppt.append(img)
-				ans_range.append((index_start,index))
-			else:
-				slides.append(img)
-			index = index + 1
-	slide_index = 0
-	for img in slides:
-		mx = 10.0
-		idx = -1
-		ppt_index = 0
-		for possibility in ppt:
-			cur_max = max(np.corrcoef(img, possibility))
-			if cur_max > mx:
-				mx = cur_max
-				idx = ppt_index
-			ppt_index = ppt_index + 1
-		if ans_range[idx][0] > slide_index or ans_range[idx][1] < slide_index:
-			print("Error : ",img)
-		slide_index = slide_index + 1
+    slides = []
+    ppt = []
+    ans_range = []
+    index = 0
+    for folder in os.listdir('Dataset'):
+        index_start = index
+        for filename in os.listdir('Dataset/'+folder):
+            img = cv.imread(os.path.join("Dataset",folder,filename))
+            # print(filename, folder)
+            imgray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+            print(img.shape, folder, filename)  
+            w,h,_=img.shape
+            edgy = edge_detection(imgray)
+            img = crop_image(edgy,w,h)
+            if filename == "ppt.jpg":
+                ppt.append(img)
+                ans_range.append((index_start,index))
+            else:
+                slides.append(img)
+            index = index + 1
+    slide_index = 0
+    for img in slides:
+        mx = 10.0
+        idx = -1
+        ppt_index = 0
+        for possibility in ppt:
+            cur_max = max(np.corrcoef(img, possibility))
+            if cur_max > mx:
+                mx = cur_max
+                idx = ppt_index
+            ppt_index = ppt_index + 1
+        if ans_range[idx][0] > slide_index or ans_range[idx][1] < slide_index:
+            print("Error : ",img)
+        slide_index = slide_index + 1
 testing()
